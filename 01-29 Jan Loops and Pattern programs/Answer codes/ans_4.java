2688428db698b56eb4c53d20a9d4da3b0c049f04
km-gupta <kmgupta.official@gmail.com>
2023-06-18 21:26:56 +0530

    assignment addition

--- 01-29 Jan Loops and Pattern programs/Answer codes/ans_4.java
+++ 01-29 Jan Loops and Pattern programs/Answer codes/ans_4.java
@@ -1,5 +1,100 @@
 public class ans_4 {
     public static void main(String[] args) {
-        System.out.println("*");
+        int row = 9;
+        int col = 70;
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                //Condition for P
+                if(j<row) {
+                    if(i==row*0 || j==row*0 || i==row/2  || (j==(row-1) && i<row/2)) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                
+                if(j==row*1) {
+                    System.out.print("  ");
+                }
+
+                //Condition for W
+                if(j>row*1 && j<row*2+2) {
+                    if(j==row*1+1 || j==row*2+1 || (i+j==row*2+1 && i>row/2) || (i==j-(row*1+1) && i>row/2)) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+
+                if(j==row*2+2) {
+                    System.out.print("    ");
+                }
+
+                //Condition for S
+                if(j>row*2+2 && j<row*3+4) {
+                    if(i==0 || j==row*2+3 && i<row/2 || i==row/2 || j==row*3+3 && i>row/2 ||  i==row-1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+
+                if(j==row*3+4) {
+                    System.out.print("  ");
+                }
+
+                //Condition for K
+                if(j>row*3+4 && j<row*3+row/2+7) {
+                    if(j==row*3+5 || i+j==row*3+row/2+6 && i<row/2 || i==j-(row*3+2) && i>=row/2) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+
+                if(j==row*3+row/2+7) {
+                    System.out.print("  ");
+                }
+
+                //Condition for I
+                if(j>row*3+row/2+7 && j<row*4+row/2+9) {
+                    if(i==0 || j==row*3+row+7 || i==row-1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+
+                if(j==row*4+row/2+9) {
+                    System.out.print("  ");
+                }
+
+                //Condition for L
+                if(j>row*4+row/2+9 && j<row*5+row/2+11) {
+                    if(j==row*4+row/2+10 || i==row-1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+
+                if(j==row*5+row/2+11) {
+                    System.out.print("  ");
+                }
+
+                //Condition for L
+                if(j>row*5+row/2+11 && j<row*6+row/2+13) {
+                    if(j==row*5+row/2+12 || i==row-1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                
+                
+            }
+            System.out.println();
+        }
+        
     }
 }
--- 01-29 Jan Loops and Pattern programs/Answer codes/ans_5.java
+++ 01-29 Jan Loops and Pattern programs/Answer codes/ans_5.java
@@ -1,5 +1,45 @@
 public class ans_5 {
     public static void main(String[] args) {
-        System.out.println("*");
+        int row = 10;
+        int col = 70;
+
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                //Condition for K
+                if(j<row/2+2) {
+                    if(j==row*0 || i+j==row*0+row/2+1 && i<row/2 || j==i-(row/2-1) && i>=row/2) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+
+                if(j==row/2+2) {
+                    if(i==row-1) {
+                        System.out.print(" * ");
+                    } else {
+                        System.out.print("   ");
+                    }
+                }
+
+                //Condition for M
+                if(j>row/2+2 && j<3*row/2+4) {
+                    if(j==row/2+3 || i==j-(row/2+3) && i<=row/2 || i+j==3*row/2+3 && i<=row/2 || j==3*row/2+3) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+
+                if(j==3*row/2+4) {
+                    if(i==row-1) {
+                        System.out.print(" * ");
+                    } else {
+                        System.out.print("   ");
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 }
